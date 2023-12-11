a66d3487e4313a0c2658811b32ce4c0d53437989
Kaushal Kawade <kaushal.kawade@oracle.com>
2023-12-11 10:50:54 +0530

    transaction is done here amount is transfered from account a->b successfully I am not yet stored transaction history

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,19 +12,21 @@ export class LoginComponent {
   name: string = '';
   pass: string = '';
   wrong: string = '';
+  flg:boolean  = false;
   constructor(private authService: AuthService, private router: Router) {}
 
-  login() {
+  login() { 
     this.authService.login(this.name, this.pass).subscribe(
       (response) => {
         //method login return response which passed in .subscribe.
         // Authentication successful, handle the response as needed
         console.log('Login successful', response);
+        this.wrong = 'invalid credentials'
+        this.flg = this.authService.isLoggedIn();
       },
       (error) => {
         // Authentication failed, handle the error
         console.error('Login failed', error);
-        this.wrong = 'invalid credentials'
       }
     );
   }
